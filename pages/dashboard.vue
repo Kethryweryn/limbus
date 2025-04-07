a4c0e72c325ac289f35dfa59fce983178177b4e4
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-07 21:18:04 +0200

    logout

---
+++ pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Tableau de bord</h1>
+    <UButton color="red" @click="logout">Se déconnecter</UButton>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: ['auth']
+});
+
+const logout = async () => {
+  await $fetch('/api/auth/logout', {
+    method: 'POST'
+  });
+
+  return navigateTo('/login');
+};
+</script>
