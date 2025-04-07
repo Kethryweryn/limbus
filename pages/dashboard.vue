ef5606cb4f88883e87b5498febc2f2ab05c34a95
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-08 00:52:38 +0200

    test

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="p-6">
-    <h1 class="text-2xl font-bold mb-4">Tableau de bord</h1>
+    <h1 class="text-2xl font-bold mb-4">
+      Tableau de bord <span v-if="user">({{ user.role }})</span>
+    </h1>
     <UButton color="red" @click="logout">Se déconnecter</UButton>
   </div>
 </template>
 
 <script setup>
-const logout = async () => {
-  await $fetch('/api/auth/logout', {
-    method: 'POST'
-  });
+const user = useRequestEvent()?.context.authUser
 
-  return navigateTo('/login');
-};
+const logout = async () => {
+  await $fetch('/api/auth/logout', { method: 'POST' })
+  return navigateTo('/login')
+}
 </script>
