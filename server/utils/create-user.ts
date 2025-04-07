666c3a6e4a3c4d0e0f67dac2cf57a920b4460193
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-08 01:09:21 +0200

    pouf

---
+++ server/utils/create-user.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+async function createUser() {
+  const email = 'kethryweryn@gmail.com'
+  const password = '01240134'
+  const name = 'Jean-Damien'
+  const role = 'orga'
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+      role
+    }
+  })
+
+  console.log('✅ Utilisateur créé :', user)
+}
+
+createUser()
+  .catch((e) => {
+    console.error('❌ Erreur :', e)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
