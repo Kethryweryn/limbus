a70490e7d1e49881209ee95073f83731e66993d7
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-08 00:00:15 +0200

    test encore et tjs

--- server/api/auth/me.get.ts
+++ server/api/auth/me.get.ts
@@ -1,24 +1,27 @@
 // server/api/auth/me.get.ts
-import jwt from 'jsonwebtoken';
-import { parseCookies } from 'h3';
+import jwt from 'jsonwebtoken'
+import { parse } from 'cookie'
 
-const SECRET = 'limbus-super-secret';
+const SECRET = 'limbus-super-secret'
 
 export default defineEventHandler(async (event) => {
     console.log('[auth/me] HEADERS DEBUG:', event.req.headers)
-    const cookies = parseCookies(event);
-    const token = cookies.limbus_token;
-    console.log('[auth/me] cookies:', cookies);
-    console.log('[auth/me] token:', token);
+
+    const rawCookie = event.req.headers.cookie || ''
+    const cookies = parse(rawCookie)
+    const token = cookies.limbus_token
+
+    console.log('[auth/me] cookies:', cookies)
+    console.log('[auth/me] token:', token)
 
     if (!token) {
-        return { authenticated: false };
+        return { authenticated: false }
     }
 
     try {
-        const payload = jwt.verify(token, SECRET);
-        return { authenticated: true, user: payload };
+        const payload = jwt.verify(token, SECRET)
+        return { authenticated: true, user: payload }
     } catch (e) {
-        return { authenticated: false };
+        return { authenticated: false }
     }
-});
+})
