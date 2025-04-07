a73e459d74e923382dca9334d04605e0c6208437
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-07 23:53:17 +0200

    encore et tjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    devProxy: false,
+    preset: 'node-server' // 👈 force l'utilisation du serveur Node natif
+  },
   compatibilityDate: '2025-04-07',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui'],
