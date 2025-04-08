b12f835421f99c82d4535e7426a51e9fed682508
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-08 18:30:41 +0200

    test

--- pages/games/[slug].vue
+++ pages/games/[slug].vue
@@ -2,7 +2,7 @@
     <div class="p-6 max-w-4xl mx-auto space-y-6">
         <h1 class="text-3xl font-bold">{{ game.title }}</h1>
 
-        <div v-if="game.teaserUrl" class="flex justify-center">
+        <div v-if="game.teaserUrl" class="justify-center">
             <div class="h-[480px] aspect-video">
                 <iframe :src="embedTeaser(game.teaserUrl)" class="w-full h-full rounded" frameborder="0"
                     allowfullscreen></iframe>
