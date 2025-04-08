123581e665d0afd9137d0bad54ae246cbb298b2b
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-08 03:34:01 +0200

    refactor

--- pages/dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1 class="text-3xl font-bold mb-6">Bienvenue sur le panneau d’administration</h1>
-    <p class="text-lg">Utilisez le menu de gauche pour naviguer entre les sections.</p>
-  </div>
-</template>
