58ac99ab402c9aba8ae9ea5c79fc7c9944745a48
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-09 01:07:36 +0200

    ajout aspect ratio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,13 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        auto: 'auto',
+        square: '1 / 1',
+        video: '16 / 9'
+      }
+    },
   },
   plugins: [],
 } satisfies Config
-
