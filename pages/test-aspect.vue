0f4ca20b48db59ae9bddb2537054a57b2303934c
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-09 00:46:57 +0200

    test

--- pages/test-aspect.vue
+++ pages/test-aspect.vue
@@ -1,12 +1,3 @@
 <template>
-    <div class="p-10">
-        <h1 class="text-2xl font-bold mb-6">Test aspect-video</h1>
-
-        <div class="w-full max-w-2xl aspect-video bg-blue-200 rounded overflow-hidden border mx-auto">
-            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen
-                class="w-full h-full"></iframe>
-        </div>
-
-        <p class="mt-6 text-center text-gray-600">Tu devrais voir une vidéo YouTube en 16:9 dans un cadre bleu.</p>
-    </div>
+    <div class="w-96 aspect-video bg-blue-500"></div>
 </template>
