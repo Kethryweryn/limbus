d6613052827bf2473eedfd133739bdef02a4304d
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-13 23:52:51 +0200

    la FATIGUE des fois

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+const router = useRouter()
+const loading = ref(true)
+const authenticated = ref(false)
+
+const checkAuth = async () => {
+  try {
+    const { data } = await useFetch('/api/auth/me')
+    authenticated.value = data.value?.authenticated
+  } catch {
+    authenticated.value = false
+  } finally {
+    loading.value = false
+    if (!authenticated.value && router.currentRoute.value.path !== '/login') {
+      router.replace('/login')
+    }
+  }
+}
+
+onMounted(() => {
+  checkAuth()
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div v-if="loading"></div>
+  <div v-else-if="authenticated">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
