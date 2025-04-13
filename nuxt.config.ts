ec0a3c95d511c04bc2a68d4768ce9a97d8147229
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 01:22:43 +0200

    Allez on retete

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-04-07',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@pinia/nuxt'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@pinia/nuxt', '@vite-pwa/nuxt'],
   ui: {
     icons: ['heroicons']
   },
@@ -17,5 +17,36 @@ export default defineNuxtConfig({
   serverMiddleware: [
     '~/server/middleware/auth-global.server.ts',
     '~/server/middleware/ignore-bots.ts'
-  ]
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    includeAssets: ['offline.html'],
+    manifest: {
+      name: 'Limbus',
+      short_name: 'Limbus',
+      description: 'Application GN utilisable hors-ligne',
+      lang: 'fr',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      //globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      navigateFallback: 'offline.html',
+      navigateFallbackDenylist: [/^\/api/]
+    }
+  }
 })
