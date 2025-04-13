e5f854d03db333fc696225fb7623f6957d096e76
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-13 22:18:26 +0200

    on va tester comme ça

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      navigateFallback: 'offline.html',
+      navigateFallback: '/public/offline.html',
       navigateFallbackDenylist: [/^\/api/]
     }
   }
