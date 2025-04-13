2427d485cc26e66172082ad7939abd82970ff63c
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-13 22:33:15 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,9 +43,9 @@ export default defineNuxtConfig({
       ]
     },
     workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      navigateFallback: 'public/offline.html',
-      //navigateFallbackDenylist: [/^\/api/]
+      //globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      navigateFallback: 'offline.html',
+      navigateFallbackDenylist: [/^\/api/]
     }
   }
 })
