6ec72ec0925446552ccbc86edcb2b10def74d6a9
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 00:45:17 +0200

    menu vers dashboard

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,7 +43,7 @@ import { navigateTo } from '#app'
 const mobileOpen = ref(false)
 
 const navLinks = [
-    { label: 'Dashboard', to: '/dashboard', icon: 'i-heroicons-home' },
+    { label: 'Dashboard', to: '/', icon: 'i-heroicons-home' },
     { label: 'Jeux', to: '/games', icon: 'i-heroicons-cube' },
     { label: 'Personnages', to: '/characters', icon: 'i-heroicons-identification' }
 ]
