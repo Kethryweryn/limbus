197045324b5f825057ab020e59a5653ae2bac621
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-13 22:58:16 +0200

    bah on teste

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
   ],
   pwa: {
     registerType: 'autoUpdate',
+    includeAssets: ['offline.html'],
     manifest: {
       name: 'Limbus',
       short_name: 'Limbus',
@@ -44,7 +45,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       //globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      navigateFallback: 'public/offline.html',
+      navigateFallback: 'offline.html',
       navigateFallbackDenylist: [/^\/api/]
     }
   }
