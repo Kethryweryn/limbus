69a3add8bd81d212949838160e80eb2b13f3a167
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 00:38:31 +0200

    Ok je comprends pas pourquoi

--- server/middleware/auth-global.server.ts
+++ server/middleware/auth-global.server.ts
@@ -11,14 +11,14 @@ export default defineEventHandler((event) => {
   // Ignorer uniquement les API d'authentification
   if (url.startsWith('/api/auth')) return
 
-  // Reload si non authentifié
-  if (!isAuthenticated(event)) {
-    return redirect(event, '/')
-  }
+  // // Reload si non authentifié
+  // if (!isAuthenticated(event)) {
+  //   return redirect(event, '/')
+  // }
 
-  // Stocker l'utilisateur dans le contexte si on veut l'exploiter ailleurs
-  const rawUser = getAuthUser(event)
-  event.context.authUser = rawUser ? JSON.parse(JSON.stringify(rawUser)) : null
+  // // Stocker l'utilisateur dans le contexte si on veut l'exploiter ailleurs
+  // const rawUser = getAuthUser(event)
+  // event.context.authUser = rawUser ? JSON.parse(JSON.stringify(rawUser)) : null
 
   // Exemple : restreindre les pages /admin à "orga"
   if (url.startsWith('/admin') || url.startsWith('/organizer')) {
