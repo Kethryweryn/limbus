4fb637bb9b17105e5201446bf24cb37b2223ca30
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 04:43:05 +0200

    Logique d'auth à nouveau

--- app.vue
+++ app.vue
@@ -1,11 +1,33 @@
 <template>
-  <!-- <div v-if="loading"></div>
-  <div v-else-if="authenticated"> -->
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <!-- </div>
+  <div v-if="loading"></div>
+  <div v-else-if="authenticated">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
   <div v-else>
     <LoginScreen />
-  </div> -->
+  </div>
 </template>
+
+<script setup lang="ts">
+import LoginScreen from '~/components/LoginScreen.vue'
+
+const loading = ref(true)
+const authenticated = ref(false)
+
+const checkAuth = async () => {
+  try {
+    const { data } = await useFetch('/api/auth/me')
+    authenticated.value = data.value?.authenticated
+  } catch {
+    authenticated.value = false
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  checkAuth()
+})
+</script>
