fdb1ff586e64f73c17f8df3d8d4046bcb553f84e
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 07:57:04 +0200

    test ?

--- app.vue
+++ app.vue
@@ -1,40 +1,31 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
-<!--
-<template>
   <div v-if="loading"></div>
   <div v-else-if="authenticated">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <NuxtRouteAnnouncer />
   </div>
   <div v-else>
     <LoginScreen />
   </div>
-</template> -->
+</template>
 
 <script setup lang="ts">
-// import LoginScreen from '~/components/LoginScreen.vue'
+import LoginScreen from '~/components/LoginScreen.vue'
 
-// const loading = ref(true)
-// const authenticated = ref(false)
+const loading = ref(true)
+const authenticated = ref(false)
 
-// const checkAuth = async () => {
-//   try {
-//     const { data } = await useFetch('/api/auth/me')
-//     authenticated.value = data.value?.authenticated
-//   } catch {
-//     authenticated.value = false
-//   } finally {
-//     loading.value = false
-//   }
-// }
+const checkAuth = async () => {
+  try {
+    const { data } = await useFetch('/api/auth/me')
+    authenticated.value = data.value?.authenticated
+  } catch {
+    authenticated.value = false
+  } finally {
+    loading.value = false
+  }
+}
 
-// onMounted(() => {
-//   checkAuth()
-// })
+onMounted(() => {
+  checkAuth()
+})
 </script>
