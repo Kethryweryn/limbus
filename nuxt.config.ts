737a532087c93694fb648e49ef70a3a4a5e60ac1
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 14:29:42 +0200

    Rajout de pinia et vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,45 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-04-07',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@pinia/nuxt', '@vite-pwa/nuxt'],
   ui: {
     icons: ['heroicons']
   },
   devServer: {
     host: '0.0.0.0',
     port: 3000,
+  },
+  serverMiddleware: [
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    includeAssets: ['offline.html'],
+    manifest: {
+      name: 'Limbus',
+      short_name: 'Limbus',
+      description: 'Application GN utilisable hors-ligne',
+      lang: 'fr',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      //globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      navigateFallback: 'offline.html',
+      navigateFallbackDenylist: [/^\/api/]
+    }
   },
 })
