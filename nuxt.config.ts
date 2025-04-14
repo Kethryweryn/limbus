443f048e14022fda8ef3b7c106878ee540511ab0
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 04:38:04 +0200

    tet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,37 +14,4 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
     port: 3000,
   },
-  serverMiddleware: [
-  ],
-  pwa: {
-    registerType: 'autoUpdate',
-    includeAssets: ['offline.html'],
-    manifest: {
-      name: 'Limbus',
-      short_name: 'Limbus',
-      description: 'Application GN utilisable hors-ligne',
-      lang: 'fr',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      icons: [
-        {
-          src: '/pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      //globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      navigateFallback: 'offline.html',
-      navigateFallbackDenylist: [/^\/api/]
-    }
-  },
 })
