d9a2b4e68a54cb6e4c1d782d34d04e3dba86bbba
Jean-Damien Mottot <jd.mottot@gmail.com>
2025-04-14 07:53:05 +0200

    test

--- app.vue
+++ app.vue
@@ -1,33 +1,40 @@
 <template>
-  <!-- <div v-if="loading"></div>
-  <div v-else-if="authenticated"> -->
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <!-- </div>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<!--
+<template>
+  <div v-if="loading"></div>
+  <div v-else-if="authenticated">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
   <div v-else>
     <LoginScreen />
-  </div> -->
-</template>
+  </div>
+</template> -->
 
-<!-- <script setup lang="ts">
-import LoginScreen from '~/components/LoginScreen.vue'
+<script setup lang="ts">
+// import LoginScreen from '~/components/LoginScreen.vue'
 
-const loading = ref(true)
-const authenticated = ref(false)
+// const loading = ref(true)
+// const authenticated = ref(false)
 
-const checkAuth = async () => {
-  try {
-    const { data } = await useFetch('/api/auth/me')
-    authenticated.value = data.value?.authenticated
-  } catch {
-    authenticated.value = false
-  } finally {
-    loading.value = false
-  }
-}
+// const checkAuth = async () => {
+//   try {
+//     const { data } = await useFetch('/api/auth/me')
+//     authenticated.value = data.value?.authenticated
+//   } catch {
+//     authenticated.value = false
+//   } finally {
+//     loading.value = false
+//   }
+// }
 
-onMounted(() => {
-  checkAuth()
-})
-</script> -->
+// onMounted(() => {
+//   checkAuth()
+// })
+</script>
